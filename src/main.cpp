8bbb753002cda294e77c24fefa7ee190e33140bc
lparolari <luca.parolari23@gmail.com>
2021-09-12 16:35:07 +0200

    style: 4-spaces indent

--- src/main.cpp
+++ src/main.cpp
@@ -39,90 +39,90 @@ aw109::rtu::Buffer<int, 0, 2> btndx3_buffer(0, (int[]){1, -1});
 
 void setup()
 {
-  Serial.begin(9600);
-  begin_components();
+    Serial.begin(9600);
+    begin_components();
 }
 
 void loop()
 {
-  tick_components();
+    tick_components();
 
-  timer.tick(millis());
+    timer.tick(millis());
 
-  update_component_buffers();
+    update_component_buffers();
 
-  if (timer.is_expired())
-  {
-    timer.reset();
+    if (timer.is_expired())
+    {
+        timer.reset();
 
-    String msg = aw109::rtu::protocol::get_notify_message(
-        brightness.get(),
-        inner_rotary_buffer.get(),
-        outer_rotary_buffer.get(),
-        btnsx1_buffer.get(),
-        btnsx2_buffer.get(),
-        btnsx3_buffer.get(),
-        btnsx4_buffer.get(),
-        btndx1_buffer.get(),
-        btndx2_buffer.get(),
-        btndx3_buffer.get());
+        String msg = aw109::rtu::protocol::get_notify_message(
+            brightness.get(),
+            inner_rotary_buffer.get(),
+            outer_rotary_buffer.get(),
+            btnsx1_buffer.get(),
+            btnsx2_buffer.get(),
+            btnsx3_buffer.get(),
+            btnsx4_buffer.get(),
+            btndx1_buffer.get(),
+            btndx2_buffer.get(),
+            btndx3_buffer.get());
 
-    aw109::rtu::protocol::send(msg);
+        aw109::rtu::protocol::send(msg);
 
-    reset_component_buffers();
-  }
+        reset_component_buffers();
+    }
 }
 
 void begin_components()
 {
-  brightness.begin();
-  inner_rotary.begin();
-  outer_rotary.begin();
-  btnsx1.begin();
-  btnsx2.begin();
-  btnsx3.begin();
-  btnsx4.begin();
-  btndx1.begin();
-  btndx2.begin();
-  btndx3.begin();
+    brightness.begin();
+    inner_rotary.begin();
+    outer_rotary.begin();
+    btnsx1.begin();
+    btnsx2.begin();
+    btnsx3.begin();
+    btnsx4.begin();
+    btndx1.begin();
+    btndx2.begin();
+    btndx3.begin();
 }
 
 void tick_components()
 {
-  brightness.tick();
-  inner_rotary.tick();
-  outer_rotary.tick();
-  btnsx1.tick();
-  btnsx2.tick();
-  btnsx3.tick();
-  btnsx4.tick();
-  btndx1.tick();
-  btndx2.tick();
-  btndx3.tick();
+    brightness.tick();
+    inner_rotary.tick();
+    outer_rotary.tick();
+    btnsx1.tick();
+    btnsx2.tick();
+    btnsx3.tick();
+    btnsx4.tick();
+    btndx1.tick();
+    btndx2.tick();
+    btndx3.tick();
 }
 
 void update_component_buffers()
 {
-  inner_rotary_buffer.set(inner_rotary.get());
-  outer_rotary_buffer.set(outer_rotary.get());
-  btnsx1_buffer.set(btnsx1.get());
-  btnsx2_buffer.set(btnsx2.get());
-  btnsx3_buffer.set(btnsx3.get());
-  btnsx4_buffer.set(btnsx4.get());
-  btndx1_buffer.set(btndx1.get());
-  btndx2_buffer.set(btndx2.get());
-  btndx3_buffer.set(btndx3.get());
+    inner_rotary_buffer.set(inner_rotary.get());
+    outer_rotary_buffer.set(outer_rotary.get());
+    btnsx1_buffer.set(btnsx1.get());
+    btnsx2_buffer.set(btnsx2.get());
+    btnsx3_buffer.set(btnsx3.get());
+    btnsx4_buffer.set(btnsx4.get());
+    btndx1_buffer.set(btndx1.get());
+    btndx2_buffer.set(btndx2.get());
+    btndx3_buffer.set(btndx3.get());
 }
 
 void reset_component_buffers()
 {
-  inner_rotary_buffer.reset();
-  outer_rotary_buffer.reset();
-  btnsx1_buffer.reset();
-  btnsx2_buffer.reset();
-  btnsx3_buffer.reset();
-  btnsx4_buffer.reset();
-  btndx1_buffer.reset();
-  btndx2_buffer.reset();
-  btndx3_buffer.reset();
+    inner_rotary_buffer.reset();
+    outer_rotary_buffer.reset();
+    btnsx1_buffer.reset();
+    btnsx2_buffer.reset();
+    btnsx3_buffer.reset();
+    btnsx4_buffer.reset();
+    btndx1_buffer.reset();
+    btndx2_buffer.reset();
+    btndx3_buffer.reset();
 }
